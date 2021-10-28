386d0413ce95ca2e14ba4b32b21188b5a08cfdce
caroldf07 <carolina.df@outlook.com>
2021-10-27 23:49:22 -0300

    day 13 - finished

--- day 13/Debbuging.py
+++ day 13/Debbuging.py
@@ -36,16 +36,3 @@ pages = int(input("Number of pages: "))
 word_per_page = int(input("Number of words per page: "))
 total_words = pages * word_per_page
 print(total_words)
-
-# Use a Debugger
-
-
-def mutate(a_list):
-    b_list = []
-    for item in a_list:
-        new_item = item * 2
-    b_list.append(new_item)
-    print(b_list)
-
-
-mutate([1, 2, 3, 5, 8, 13])
